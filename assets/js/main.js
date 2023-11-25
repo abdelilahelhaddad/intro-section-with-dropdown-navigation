a65dda9baea6a76f83bbc7ff8fdf95c598a97083
Kirito Msao <55633676+abdelilahelhaddad@users.noreply.github.com>
2023-11-25 16:25:07 +0100

    :iphone: :seedling: Mobile Responsive :sparkles: :iphone:

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,27 +4,39 @@ const company = document.querySelector("#company");
 const companyDropdown = document.querySelector("#companyDropdown");
 const body = document.querySelector("body");
 
-features.addEventListener("mouseover", function () {
-  featuresDropdown.style.display = "flex";
-  companyDropdown.style.display = "none";
-});
+if (window.matchMedia("(min-width: 769px)").matches) {
+  features.addEventListener("mouseover", function () {
+    featuresDropdown.style.display = "flex";
+    companyDropdown.style.display = "none";
+  });
 
-company.addEventListener("mouseover", function () {
-  companyDropdown.style.display = "flex";
-  featuresDropdown.style.display = "none";
-});
+  company.addEventListener("mouseover", function () {
+    companyDropdown.style.display = "flex";
+    featuresDropdown.style.display = "none";
+  });
 
-featuresDropdown.addEventListener("mouseout", function () {
-  featuresDropdown.style.display = "none";
-});
+  featuresDropdown.addEventListener("mouseout", function () {
+    featuresDropdown.style.display = "none";
+  });
 
-companyDropdown.addEventListener("mouseover", function () {
-  companyDropdown.style.display = "none";
-});
+  companyDropdown.addEventListener("mouseout", function () {
+    companyDropdown.style.display = "none";
+  });
+}
 
 if (window.matchMedia("(max-width: 768px)").matches) {
   featuresDropdown.classList.remove("dropdown");
+  featuresDropdown.classList.add("mobileDropdown");
   companyDropdown.classList.remove("dropdown");
+  companyDropdown.classList.add("mobileDropdown");
+
+  features.addEventListener("click", function () {
+    featuresDropdown.classList.toggle("dpNone");
+  });
+
+  company.addEventListener("click", function () {
+    companyDropdown.classList.toggle("dpNone");
+  });
 }
 
 const openMenuIcon = document.querySelector("#openMenuIcon");
